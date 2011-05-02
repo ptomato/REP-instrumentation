b54015cac542af3e12909c60f53c7db80a5b1881
Philip Chimento <philip.chimento@gmail.com>
2011-05-02 15:42:47 +0200

    Add distutils script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='REP-instrumentation',
+    version='0',
+    description='Python interfaces to lab instruments',
+    author='P. F. Chimento',
+    author_email='philip.chimento@gmail.com',
+    url='https://github.com/ptomato/REP-instrumentation',
+    license='gpl3',
+    requires=['pyvisa'],
+    py_modules=['Camera', 'ApogeeCam', 'Webcam', 'esp300'])
