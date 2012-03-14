f60688eb5fa7382b2edaad49201b0eef10b1514b
Philip Chimento <philip.chimento@gmail.com>
2012-03-14 12:13:50 +0100

    [OceanOptics] implement USB2000+ reading
    Also, move some common functionality to the base class

--- OceanOptics.py
+++ OceanOptics.py
@@ -127,8 +127,6 @@ class OceanOptics(object):
 
 		# Initialize
 		vpp43.write(self._vi, '\x01')  # Reset command
-		# Reset must be followed by reading the acquired spectrum
-		self._read_spectrum_data()
 	
 	def close(self):
 		vpp43.close(self._vi)
@@ -155,6 +153,11 @@ class OceanOptics(object):
 		answer = vpp43.read(self._vi, 18)
 		return answer[2:answer.find('\x00', 2)] # from byte 3 to the next null
 
+	def read_spectrum(self):
+		# Request spectrum
+		vpp43.write(self._vi, '\x09')
+		return self._read_spectrum_data()
+
 	def _read_spectrum_data(self):
 		raise NotImplementedError
 
@@ -176,6 +179,10 @@ class OceanOptics(object):
 		return a + b * p + c * p ** 2.0 + d * p ** 3.0
 
 	@property
+	def num_pixels(self):
+		return struct.unpack('<H', self._query_status()[0:2])[0]
+
+	@property
 	def integration_time(self):
 	    raise NotImplementedError
 	    
@@ -183,18 +190,33 @@ class OceanOptics(object):
 	def integration_time(self, value):
 	    raise NotImplementedError
 
+	@property
+	def lamp_enabled(self):
+		return NotImplementedError
+
+	@property
+	def trigger_mode_value(self):
+		return NotImplementedError
+
+	@property
+	def data_ready(self):
+	    """Whether data are available."""
+	    return self._query_status()[8] != '\x00'
+
 class OceanOptics2k(OceanOptics):
 	_in_pipe = 0x87
 	_out_pipe = 0x02
 	_min_integration_time = 3e-3
+	max_counts = 4096
 
 	def __init__(self, *args, **kwargs):
 		OceanOptics.__init__(self, *args, **kwargs)
 	
-	@property
-	def num_pixels(self):
-		return int(self._query_status()[0:2].encode('hex'), 16)
-	
+	def open(self):
+		OceanOptics.open(self)
+		# Reset must be followed by reading the acquired spectrum
+		self._read_spectrum_data()
+
 	@property
 	def integration_time(self):
 	    """Integration time in seconds"""
@@ -227,17 +249,6 @@ class OceanOptics2k(OceanOptics):
 	def timer_swap(self):
 	    """16-bit timer for integration time (0) or 8-bit timer (1)."""
 	    return ord(self._query_status()[7])
-	
-	@property
-	def data_ready(self):
-	    """Whether data are available."""
-	    return self._query_status()[8] != '\x00'
-	
-	def read_spectrum(self):
-
-		# Request spectrum
-		vpp43.write(self._vi, '\x09')
-		return self._read_spectrum_data()
 
 	def _read_spectrum_data(self):
 
@@ -286,19 +297,17 @@ class OceanOptics4k(OceanOptics):
 	_in_pipe = 0x81
 	_out_pipe = 0x01
 	_min_integration_time = 1e-5
+	max_counts = 65536
 
 	def __init__(self, *args, **kwargs):
 		OceanOptics.__init__(self, *args, **kwargs)
-
-		# Query USB speed
-		#usb_speed = ord(self._query_status()[14])
-		#self._usb_speed = 'high' if usb_speed == 128 else 'full'
-		# 128 = High, 480 Mbps; 0 = Full, 12 Mbps
+		self._usb_speed = None
+		self._saturation_level = None
 
 	@property
 	def integration_time(self):
 	    """Integration time in seconds"""
-	    return int(self._query_status()[2:6].encode('hex'), 16) * 1e-6
+	    return struct.unpack('<I', self._query_status()[2:6])[0] * 1e-6
 
 	@integration_time.setter
 	def integration_time(self, value):
@@ -307,6 +316,74 @@ class OceanOptics4k(OceanOptics):
 				'{}'.format(self._min_integration_time))
 		packed_value = struct.pack('<I', value * 1e6)
 		vpp43.write(self._vi, '\x02' + packed_value)
+
+	@property
+	def lamp_enabled(self):
+	    """Whether the lamp signal is HIGH (True) or LOW (False)."""
+	    return self._query_status()[6] != '\x00'
+	    
+	@property
+	def trigger_mode_value(self):
+	    """Whatever this is"""
+	    return ord(self._query_status()[7])
+
+	@property
+	def num_packets(self):
+		"""Number of packets in spectrum"""
+		return ord(self._query_status()[9])
+
+	@property
+	def power_on_status(self):
+		"""Whatever this is"""
+		return ord(self._query_status()[10])
+
+	@property
+	def packet_count(self):
+		"""Whatever this is"""
+		return ord(self._query_status()[11])
+
+	@property
+	def usb_speed(self):
+		"""USB speed, 'high' = 480 Mbps; 'full' = 12 Mbps"""
+		usb_speed = ord(self._query_status()[14])
+		# Cache the result
+		self._usb_speed = 'high' if usb_speed == 128 else 'full'
+		return self._usb_speed
+
+	def _read_spectrum_data(self):
+		usb_speed = self._usb_speed
+		# Only query the value if there is no cached value
+		if usb_speed is None:
+			usb_speed = self.usb_speed
+
+		n_packets, packet_length = (8, 512) if usb_speed == 'high' else (64, 64)
+		# Assign endpoint
+		vpp43.set_attribute(self._vi, VI_ATTR_USB_BULK_IN_PIPE, 0x82)
+
+		data = ''
+		for i in range(n_packets):
+			data += vpp43.read(self._vi, packet_length)
+		
+		# Read sync packet to check if properly synchronized
+		sync_byte = vpp43.read(self._vi, 1)
+		if sync_byte != '\x69' or len(data) != 4096:
+			raise visa.VisaIOError(OO_ERROR_SYNC)
+		
+		# Reassign endpoint
+		vpp43.set_attribute(self._vi, VI_ATTR_USB_BULK_IN_PIPE, 0x81)
+		
+		spectrum = N.fromstring(data, N.dtype('<u2'))
+
+		# Query and cache the saturation level if it is not cached
+		if self._saturation_level is None:
+			vpp43.write(self._vi, '\x05\x11')
+			autonull_info = vpp43.read(self._vi, 17)
+			self._saturation_level = (65536.0 
+				/ struct.unpack('<H', autonull_info[6:8])[0])
+			# Hmmm, it seems that this is an OceanOptics trick to sell
+			# spectrometers with much less dynamic range than advertised!
+
+		return spectrum * self._saturation_level
 
 class HR4000(OceanOptics4k):
 	_model_code = 4114
@@ -398,8 +475,8 @@ if __name__ == '__main__':
 					Item('serial_number', show_label=False, style='readonly',
 						format_str='%s,'),
 					Item('num_pixels', show_label=False, style='readonly',
-						format_str='%i pixels,'),
-					Item('integration_time', format_str='%e s')
+						format_str='%i pixels'),
+					Item('integration_time', label='Integration time (s)')
 				),
 				Item('graph', editor=ComponentEditor(), show_label=False)
 			),
@@ -428,7 +505,7 @@ if __name__ == '__main__':
 			graph.index_axis.title = 'Wavelength (nm)'
 			graph.value_axis.title = 'Counts'
 			graph.value_range.low_setting = 0
-			graph.value_range.high_setting = 4096
+			graph.value_range.high_setting = self._sm.max_counts
 			return graph
 
 		def _spectrum_changed(self, new_data):
@@ -439,7 +516,8 @@ if __name__ == '__main__':
 			self.integration_time = self._sm.integration_time
 
 		def _update_plot(self):
-			self.spectrum = self._sm.read_spectrum()
+			if not self._sm.data_ready:
+				self.spectrum = self._sm.read_spectrum()
 
 		def configure_traits(self, *args, **kw):
 			# Start the timer when showing the window
